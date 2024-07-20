b3c7ebbd3ab58b596d4c7fc61f1091e019e7d2b4
James Adam <zigisgreat@proton.me>
2024-07-20 19:07:38 -0400

    Add Language Basics examples 10-13
    This includes the following:
    - runtime safety
    - unreachable
    - pointers
    - pointer-sized ints (usize/isize)

--- examples/01 Language Basics/11 unreachable.zig
+++ examples/01 Language Basics/11 unreachable.zig
@@ -1,3 +1,25 @@
 const std = @import("std");
+const expectEqual = std.testing.expectEqual;
 
-pub fn main() !void {}
+fn asciiToUpper(x: u8) u8 {
+    return switch (x) {
+        'a'...'z' => x + 'A' - 'a',
+        'A'...'Z' => x,
+        // Using `unreachable` here tells the compiler that this branch
+        // is impossible, which the optimiser can take advantage of.
+        else => unreachable,
+    };
+}
+
+test "unreachable switch" {
+    try expectEqual('R', asciiToUpper('r'));
+    try expectEqual('C', asciiToUpper('C'));
+}
+
+test "unreachable" {
+    const x: u8 = 5;
+    // Since `unreachable` is of type `noreturn`, it can
+    // coerce to `u8` no problem.
+    const y: u8 = if (x == 4) 3 else unreachable;
+    _ = y;
+}

33d191bb55babb6a4867e5c4db5508e5762f7bbe
James Adam <zigisgreat@proton.me>
2024-07-28 14:08:12 -0400

    Rewrite the arrays example to have much more information
    I realized this example didn't have a ton of information to offer as it
    was, so I decided to completely rewrite it. It now covers:
    
    - Array / string literals
    - Iteration
    - Modification
    - Concatenation with '++'
    - Multiplication with '**'
    - Zero initialization
    - Initialization with functions

--- examples/01 Language Basics/02 arrays.zig
+++ examples/01 Language Basics/02 arrays.zig
@@ -1,15 +1,106 @@
 const std = @import("std");
-const stdout = std.io.getStdOut().writer();
 
-pub fn main() !void {
-    // using a comptime-known value for the size
-    const a = [12]u8{ 72, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100, 33 };
-    // for array literals, '_' lets the compiler infer the size
-    const b = [_]u8{ 'B', 'y', 'e', '!' };
+// Initialization using an array literal:
+const hello = [_]u8{ 'H', 'e', 'l', 'l', 'o' };
 
-    // now we are printing to stdout instead of stderr
-    try stdout.print("{s} {s}\n", .{ a, b });
+// Using result location:
+const alt_hello: [5]u8 = .{ 'H', 'e', 'l', 'l', 'o' };
 
-    // 'len' holds the arrays' length
-    try stdout.print("The length of a is {d}.\n", .{a.len});
+// Using a string literal:
+const same_hello = "Hello";
+
+comptime {
+    // All of them are all the same
+    std.debug.assert(std.mem.eql(u8, &hello, &alt_hello));
+    std.debug.assert(std.mem.eql(u8, &hello, same_hello));
+
+    // Get the size of an array
+    std.debug.assert(hello.len == 5);
+}
+
+test "iterate over an array" {
+    var sum: usize = 0;
+    for (hello) |byte| {
+        sum += byte;
+    }
+    try std.testing.expectEqual('H' + 'e' + 'l' * 2 + 'o', sum);
+}
+
+// A modifiable array:
+var items: [100]u8 = undefined;
+
+test "modifiable array" {
+    // assigning by index:
+    for (0..items.len) |i| {
+        items[i] = @intCast(i);
+    }
+    // using a many-item pointer
+    for (&items, 0..) |*item, i| {
+        item.* = @intCast(i);
+    }
+
+    try std.testing.expectEqual(10, items[10]);
+    try std.testing.expectEqual(99, items[99]);
+}
+
+// Array concatenation works if the values are known at compile time:
+const array_one = [_]u32{ 1, 2, 3, 4 };
+const array_two = [_]u32{ 5, 6, 7, 8 };
+const both = array_one ++ array_two;
+comptime {
+    std.debug.assert(std.mem.eql(u32, &both, &[_]u32{ 1, 2, 3, 4, 5, 6, 7, 8 }));
+}
+
+// Remember that string literals are also arrays:
+const world = "world";
+const hello_world = hello ++ " " ++ world;
+
+// Use '**' to repeat elements:
+const pattern = "zig" ** 3;
+comptime {
+    std.debug.assert(std.mem.eql(u8, pattern, "zigzigzig"));
+}
+
+// Initialize an array to zero:
+const zeroes = [_]u8{0} ** 10;
+comptime {
+    std.debug.assert(zeroes.len == 10);
+    std.debug.assert(zeroes[5] == 0);
+}
+
+const Point = struct {
+    x: u32,
+    y: u32,
+};
+
+// Initialize an array at compile-time:
+var comptime_array = blk: {
+    var initial_value: [10]Point = undefined;
+    for (&initial_value, 0..) |*pt, i| {
+        pt.* = Point{
+            .x = @intCast(i),
+            .y = @intCast(i * 2),
+        };
+    }
+    break :blk initial_value;
+};
+
+test "comptime array initialization" {
+    try std.testing.expectEqual(3, comptime_array[3].x);
+    try std.testing.expectEqual(6, comptime_array[3].y);
+}
+
+fn makePoint(x: u32) Point {
+    return .{
+        .x = x,
+        .y = x * 2,
+    };
+}
+
+var another_array = [_]Point{makePoint(3)} ** 10;
+
+test "array initialization with function calls" {
+    try std.testing.expectEqual(3, another_array[4].x);
+    try std.testing.expectEqual(6, another_array[4].y);
+    try std.testing.expectEqual(10, another_array.len);
 }

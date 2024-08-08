e7ca42d6850e628ccb18d388286c2730d3c7f2f9
James Adam <zigisgreat@proton.me>
2024-08-08 00:06:47 -0400

    feat: Add more information/tests to the while loops example
    The following topics are now covered:
    - labelled while
    - while with optionals
    - while with error unions
    - inline while

--- examples/01 Language Basics/04 while_loop.zig
+++ examples/01 Language Basics/04 while_loop.zig
@@ -33,3 +33,93 @@ test "while with continue expression" {
     while (d <= 32) : (d *= 2) {}
     try expectEqual(64, d);
 }
+
+test "labelled while" {
+    // Example of a nested break:
+    outer: while (true) {
+        while (true) {
+            break :outer;
+        }
+    }
+
+    // Example of a nested continue:
+    var e: u8 = 1;
+    outer: while (e <= 64) : (e *= 2) {
+        while (true) {
+            continue :outer;
+        }
+    }
+    try expectEqual(128, e);
+}
+
+test "while with optionals" {
+    var f: u8 = 0;
+    items_left = 4;
+    while (eventuallyNullSequence()) |value| {
+        f += value;
+    }
+    try expectEqual(6, f);
+
+    // null capture with an else block
+    var g: u8 = 0;
+    items_left = 4;
+    while (eventuallyNullSequence()) |value| {
+        g += value;
+    } else {
+        try expectEqual(6, g);
+    }
+
+    // null capture with a continue expression
+    var h: u8 = 0;
+    var count: u8 = 0;
+    items_left = 4;
+    while (eventuallyNullSequence()) |value| : (count += 1) {
+        h += value;
+    }
+    try expectEqual(4, count);
+}
+
+var items_left: u8 = undefined;
+fn eventuallyNullSequence() ?u8 {
+    return if (items_left == 0) null else blk: {
+        items_left -= 1;
+        break :blk items_left;
+    };
+}
+
+test "while with error unions" {
+    var i: u8 = 0;
+    items_left = 4;
+    while (eventuallyErrorSequence()) |value| {
+        i += value;
+    } else |err| {
+        try expectEqual(error.OutOfItems, err);
+    }
+}
+
+fn eventuallyErrorSequence() !u8 {
+    return if (items_left == 0) error.OutOfItems else blk: {
+        items_left -= 1;
+        break :blk items_left;
+    };
+}
+
+test "inline while" {
+    comptime var i: u8 = 0;
+    var sum: usize = 0;
+    inline while (i < 3) : (i += 1) {
+        // Types can be used as first-class values now!
+        const T = switch (i) {
+            0 => u65535,
+            1 => comptime_int,
+            2 => anyopaque,
+            else => unreachable,
+        };
+        sum += @typeName(T).len;
+    }
+    try expectEqual(27, sum);
+}
+
+// NOTE: It's recommended to only `inline` loops for one of two reasons:
+//       - You need the loop to execute at comptime for the semantics to work.
+//       - You have a benchmark to prove that doing so is measurably faster.

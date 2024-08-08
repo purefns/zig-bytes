9078a846c6fbf1a200d43ac759fef6fa30bdb7e1
purefns <zigisgreat@proton.me>
2024-08-08 00:09:59 -0400

    Refactor while loops example and add more information (#32)
    * refactor: Simplify and re-order the tests in the while loops example
    * feat: Add more information/tests to the while loops example
    
    The following topics are now covered:
    - labelled while
    - while with optionals
    - while with error unions
    - inline while

--- examples/01 Language Basics/04 while_loop.zig
+++ examples/01 Language Basics/04 while_loop.zig
@@ -1,39 +1,125 @@
 const std = @import("std");
-const expect = std.testing.expect;
+const expectEqual = std.testing.expectEqual;
 
 test "while" {
     var a: u16 = 2;
-    while (a < 100) {
-        a *= a;
+    while (a < 8) {
+        a *= 2;
     }
-    try expect(a == 256);
-}
-
-test "while with continue expression" {
-    var sum: u16 = 0;
-    var i: u8 = 1;
-    while (i <= 42) : (i += 1) {
-        sum += i;
-    }
-    try expect(sum == 903);
-}
-
-test "while with continue keyword" {
-    var sum: u16 = 0;
-    var i: u8 = 1;
-    while (i <= 42) : (i += 1) {
-        if (i == 10) continue;
-        sum += i;
-    }
-    try expect(sum == 893);
+    try expectEqual(8, a);
 }
 
 test "while with break keyword" {
-    var sum: u16 = 0;
-    var i: u8 = 1;
-    while (i <= 42) : (i += 1) {
-        if (i == 10) break;
-        sum += i;
+    var b: u8 = 1;
+    while (true) {
+        if (b == 16) break;
+        b *= 2;
     }
-    try expect(sum == 45);
+    try expectEqual(16, b);
 }
+
+test "while with continue keyword" {
+    var c: u8 = 1;
+    while (true) {
+        c *= 2;
+        if (c <= 16) continue;
+        break;
+    }
+    try expectEqual(32, c);
+}
+
+test "while with continue expression" {
+    var d: u8 = 1;
+    while (d <= 32) : (d *= 2) {}
+    try expectEqual(64, d);
+}
+
+test "labelled while" {
+    // Example of a nested break:
+    outer: while (true) {
+        while (true) {
+            break :outer;
+        }
+    }
+
+    // Example of a nested continue:
+    var e: u8 = 1;
+    outer: while (e <= 64) : (e *= 2) {
+        while (true) {
+            continue :outer;
+        }
+    }
+    try expectEqual(128, e);
+}
+
+test "while with optionals" {
+    var f: u8 = 0;
+    items_left = 4;
+    while (eventuallyNullSequence()) |value| {
+        f += value;
+    }
+    try expectEqual(6, f);
+
+    // null capture with an else block
+    var g: u8 = 0;
+    items_left = 4;
+    while (eventuallyNullSequence()) |value| {
+        g += value;
+    } else {
+        try expectEqual(6, g);
+    }
+
+    // null capture with a continue expression
+    var h: u8 = 0;
+    var count: u8 = 0;
+    items_left = 4;
+    while (eventuallyNullSequence()) |value| : (count += 1) {
+        h += value;
+    }
+    try expectEqual(4, count);
+}
+
+var items_left: u8 = undefined;
+fn eventuallyNullSequence() ?u8 {
+    return if (items_left == 0) null else blk: {
+        items_left -= 1;
+        break :blk items_left;
+    };
+}
+
+test "while with error unions" {
+    var i: u8 = 0;
+    items_left = 4;
+    while (eventuallyErrorSequence()) |value| {
+        i += value;
+    } else |err| {
+        try expectEqual(error.OutOfItems, err);
+    }
+}
+
+fn eventuallyErrorSequence() !u8 {
+    return if (items_left == 0) error.OutOfItems else blk: {
+        items_left -= 1;
+        break :blk items_left;
+    };
+}
+
+test "inline while" {
+    comptime var i: u8 = 0;
+    var sum: usize = 0;
+    inline while (i < 3) : (i += 1) {
+        // Types can be used as first-class values now!
+        const T = switch (i) {
+            0 => u65535,
+            1 => comptime_int,
+            2 => anyopaque,
+            else => unreachable,
+        };
+        sum += @typeName(T).len;
+    }
+    try expectEqual(27, sum);
+}
+
+// NOTE: It's recommended to only `inline` loops for one of two reasons:
+//       - You need the loop to execute at comptime for the semantics to work.
+//       - You have a benchmark to prove that doing so is measurably faster.

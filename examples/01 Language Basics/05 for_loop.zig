0a9f67f5ab384978008edb926c627053e226890d
purefns <zigisgreat@proton.me>
2024-08-15 18:21:30 -0400

    Refactor for loop example (#34)
    * refactor: Nuke the for loops example
    * refactor: Rewrite the for loops example

--- examples/01 Language Basics/05 for_loop.zig
+++ examples/01 Language Basics/05 for_loop.zig
@@ -1,27 +1,81 @@
 const std = @import("std");
-const print = std.debug.print;
+const expectEqual = std.testing.expectEqual;
 
-pub fn main() void {
-    // character literals are equivalent to integer literals
-    const binary_str = [_]u8{ 90, 'i', 103, 32 };
-    const octal_str = [_]u8{ 73, 's', ' ' };
-    const hex_str = [_]u8{ 71, 'r', 101, 'a', 116, 33 };
+test "capture syntax" {
+    const items = [_]u32{ 1, 3, 5 };
 
-    // iterate over an array with an index
-    for (binary_str, 0..) |character, i| {
-        print("'{c}' in binary is:\t0b{b:0>8}\n", .{ character, binary_str[i] });
+    var sum: u32 = 0;
+    for (items) |value| {
+        sum += value;
     }
 
-    // just iterate over the array
-    for (octal_str) |character| {
-        print("'{c}' in octal is:\t0o{o:0>8}\n", .{ character, character });
+    try expectEqual(9, sum);
+}
+
+test "range syntax" {
+    var sum: usize = 0;
+
+    // Ranges in for loops are always exclusive,
+    // meaning the final number is not included:
+    for (0..5) |i| {
+        sum += i;
     }
 
-    // you can also ignore unused captures with '_'
-    for (hex_str, 0..) |_, i| {
-        print("'{c}' in hexidecimal is:\t0x{x:0>8}\n", .{ hex_str[i], hex_str[i] });
+    try expectEqual(10, sum);
+}
+
+test "index value" {
+    const items = [_]u32{ 1, 3, 5 };
+    var sum: usize = 0;
+
+    // Use an unbounded range as the second item
+    // to capture the current index of iteration:
+    for (items, 0..) |value, i| {
+        _ = value;
+        sum += i;
     }
 
-    // interesting but probably useless
-    for ("") |_| unreachable;
+    try expectEqual(3, sum);
+}
+
+test "multiple objects" {
+    const items_a = [_]usize{ 0, 1, 2 };
+    const items_b = [_]usize{ 3, 4, 5 };
+    var sum: usize = 0;
+
+    // All lengths must be equal at the start of the loop,
+    // otherwise detectable illegal behavior occurs.
+    for (items_a, items_b) |a, b| {
+        sum += a + b;
+    }
+
+    try expectEqual(15, sum);
+}
+
+test "value by reference" {
+    var items = [_]u32{ 1, 2, 3 };
+
+    // Using a pointer capture:
+    for (&items) |*value| {
+        value.* += 1;
+    }
+
+    try expectEqual(2, items[0]);
+    try expectEqual(3, items[1]);
+    try expectEqual(4, items[2]);
+}
+
+test "for else" {
+    const items = [_]u32{ 1, 7, 0, 4, 5 };
+
+    for (items) |value| {
+        if (value == 4) break;
+    } else {
+        // Since `items` does indeed contain 4,
+        // the break statement above is reached
+        // and this block is never evaluated.
+        return error.NotFound;
+    }
+
+    // do more stuff...
 }
